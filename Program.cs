5e8cf54be876afe033d66f469ec402480a9ef0e6
Valentin <burov0742@gmail.com>
2022-08-09 13:46:55 +0300

    Lesson5 added

--- Program.cs
+++ Program.cs
@@ -118,3 +118,44 @@ for (int i = 0; i < 4; i++)
 Console.WriteLine("конечный массив: [" + string.Join(", ", array) + "]");
 
 */
+
+// Lesson 5 - сортировка пузырьком O(n^2). каждый раз цикл сравниевает попартно значения и переставляет их если необходимо. 
+//После каждой перестановке цикл начинает проверку с начала массиваю
+
+/*
+Начальный массив: [3, 1, 5, 0, 7, 9, 8]
+1. [1, 3, 5, 0, 7, 9, 8]
+2. [1, 3, 0, 5, 7, 9, 8]
+3. [1, 0, 3, 5, 7, 9, 8]
+4. [0, 1, 3, 5, 7, 9, 8]
+5. [0, 1, 3, 5, 7, 8, 9]
+
+*/
+
+/*
+Console.WriteLine("введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [n];
+
+for (int i =0; i < n; i++)
+{
+    Console.Write("введите знаяния элементов: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("начальный массив: [" + string.Join(", ", array) + "]");
+Console.WriteLine();
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n-1; j++)
+    {
+        if (array[j] > array[j+1])
+        {
+            int temp = array[j];
+            array[j] = array[j+1];
+            array[j+1] = temp;
+        }
+    }
+    Console.WriteLine(i + " [" + string.Join(", ", array) + "]");
+}
+*/
