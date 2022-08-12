90937f4c4259a638a739984daae7b6d4d35b5491
Valentin <burov0742@gmail.com>
2022-08-12 11:16:45 +0300

    Lesson 6 added

--- Program.cs
+++ Program.cs
@@ -168,35 +168,27 @@ for (int i = 0; i < n; i++)
 3. вызвать шаги 1 и 2 для подмассивов справа и слева от pivot 
 */
 
-int [] arr = {0, -5, 2, 3, 5, 9, -1, 7};
-int [] result = null;
-
-int [] QuickSort (int [] inputArray, int minIndex, int maxIndex)
+public static void QuickSort (int [] imputArray, int minIndex, int maxIndex)
 {
-    int pivot = 0;
-    QuickSort (inputArray, minIndex, pivot - 1);
-    QuickSort (inputArray, pivot +1, maxIndex);
-    return inputArray;
-}
-
-int GetPivotIndex (int [] inputArray, int minIndex, int maxIndex)
-{
-    int pivotIndex = minIndex -1;
-    for (int i = minIndex; i < maxIndex - 1; i++);
+    static int GetPivotIndex (int [] inputArray, int maxIndex, int maxIndex)
     {
-        if (inputArray[i] <= inputArray[maxIndex])
+        int pivotIndex = minIndex - 1;
+        for (int i = minIndex; i <= maxIndex; i++)
         {
-            pivotIndex ++;
-            Swap (inputArray, i, pivotIndex);
+            if (inputArray[i] < inputArray[maxIndex])
+            {
+                pivotIndex++;
+                Swap (ref inputArray[i], ref inputArray[pivotIndex]);
+            }
         }
-    } 
-    pivotIndex++;
-    Swap
-}
+        pivotIndex++;
+        Swap (ref inputArray[pivotIndex], ref inputArray[maxIndex]);
+        return pivotIndex;
+    }
 
-void Swap (int [] inputArray, int leftValue, int rightValue)
-{
-    int temp = inputArray [leftValue];
-    inputArray[leftValue] = inputArray[rightValue];
-    inputArray[rightValue] = temp;
+    if (minIndex >= maxIndex) return;
+    int pivot = GetPivotIndex (inputArray, minIndex, maxIndex);
+    QuickSort (inputArray, minIndex, pivot - 1);
+    QuickSort (inputArray, pivot + 1), manIndex);
+    return;
 }
